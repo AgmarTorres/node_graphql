453838177445065000eea0f054bd5f864a2cdd92
Agmar Torres <agmartorres@gmail.com>
2020-04-30 11:52:33 -0300

    Exemplo de aplicação com graphql

---
+++ src/server.js
@@ -0,0 +1,39 @@
+//import { GraphQLServer } from 'graphql-yoga'
+const { GraphQLServer } =  require('graphql-yoga')
+const path = require('path')
+const resolvers = require('./resolvers')
+const mongoose = require( 'mongoose')
+mongoose.connect('mongodb://localhost/graphqlnode',{
+  useNewUrlParser: true,
+})
+
+  //Os Controlers são chamados de resolvers
+const server = new GraphQLServer({
+    typeDefs: path.resolve(__dirname, 'schema.graphql'),
+    resolvers
+});
+
+server.start();
+
+
+//Localhost:4000
+/* 
+Todos os Usuários
+  query{
+    users{
+      id
+      name
+      email
+    }
+  }
+
+
+   query{
+    users{
+      id
+      name
+    }
+  }
+
+
+*/
